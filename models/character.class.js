23c256183d037fe21694e1febbb22748e7b51294
Kevin Schumilo <kevuschka@outlook.com>
2023-03-23 18:47:01 +0100

    pepe idle animation

--- models/character.class.js
+++ models/character.class.js
@@ -4,8 +4,53 @@ class Character extends MovableObject {
     height = 280;
     width = 150;
 
+    IMAGES_IDLE = [
+        'img/2_character_pepe/1_idle/idle/I-1.png',
+        'img/2_character_pepe/1_idle/idle/I-2.png',
+        'img/2_character_pepe/1_idle/idle/I-3.png',
+        'img/2_character_pepe/1_idle/idle/I-4.png',
+        'img/2_character_pepe/1_idle/idle/I-5.png',
+        'img/2_character_pepe/1_idle/idle/I-6.png',
+        'img/2_character_pepe/1_idle/idle/I-7.png',
+        'img/2_character_pepe/1_idle/idle/I-8.png',
+        'img/2_character_pepe/1_idle/idle/I-9.png',
+        'img/2_character_pepe/1_idle/idle/I-10.png'
+    ]
+
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png',
+    ]
+
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.IMAGES_IDLE);
+        this.animateIdle();
+    }
+
+    currentImage = 0;
+
+    animateIdle() {
+        setInterval( () => {
+            let i = this.currentImage % this.IMAGES_IDLE.length;
+            let path = this.IMAGES_IDLE[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
+    }
+
+
+    animateWalking() {
+        setInterval( () => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
     }
 
     jump() {
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,20 @@
 class Cloud extends MovableObject {
-    x = 50;
+    x = 300;
     y = 10;
     width = 600;
     height = 300;
     
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/2.png');
+        this.animate();
     }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.12;
+        }, 1000 / 60);
+    }
+
+    
+
 }
